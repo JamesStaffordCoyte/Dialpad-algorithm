750aeed1ce5132fa8493027486588f6f89025b33
James <jstaffordcoyte@gmail.com>
2018-10-04 11:53:28 -0700

    added smart contract

---
+++ blockchain-test.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.17;
+
+contract Dialpad {
+  // state variables
+
+  // payable
+
+  // function that gets fired after 100 digits is found
+
+    // get address
+    // pay owner
+}
