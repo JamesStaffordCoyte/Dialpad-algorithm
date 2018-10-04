a8e1ac0d885724a275a7b8625f4e93c874559386
James <jstaffordcoyte@gmail.com>
2018-10-04 13:01:12 -0700

    create contract

--- blockchain-test.sol
+++ blockchain-test.sol
@@ -1,12 +1,45 @@
 pragma solidity ^0.4.17;
 
-contract Dialpad {
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import "./Ownable.sol";
+
+contract Dialpad is Ownable {
   // state variables
+  uint256 public bounty;
+  address public owner;
+  uint256 digits = 100;
+  address public winner;
 
-  // payable
+  // self-destruct function
+  function kill() public {
+    if (msg.sender == owner) selfdestruct(owner);
+  }
 
-  // function that gets fired after 100 digits is found
+  // Sets owner of contract and bounty (0.1 Eth must be sent )
+  constructor() public {
+    owner = msg.sender;
+  }
 
-    // get address
-    // pay owner
+  // Only owner can set bounty
+  function setBounty() external onlyOwner payable{
+    bounty = msg.value;
+  }
+
+  // Called from Javascript with web3.js if the user has found the 100 digit answer
+  function lockFundsUntil (uint256 _count) public {
+    require(_count === digits);
+    winner = msg.sender;
+    // Call withdraw
+    withdraw()
+  }
+
+  function withdraw () internal payable{
+    require(msg.sender === winner);
+    uint256 payout = bounty;
+    bounty = 0;
+    msg.sender.transfer(payout);
+ }
+
+ function () public payable {}
+
 }
